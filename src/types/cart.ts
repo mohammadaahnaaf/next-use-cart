cbb199b8e30d9899513dac394f6c4b00873758b2
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2024-07-12 22:58:30 +0600

    version 1.0.3

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,14 +1,20 @@
 export interface CartItem {
-    id: number;
-    name: string;
-    qty: number;
-  }
-  
-  export interface CartContextType {
-    cart: CartItem[];
-    addItem: (item: CartItem) => void;
-    removeItem: (id: number) => void;
-    updateItem: (id: number, qty: number) => void;
-    getTotalItemCount: () => number;
-  }
-  
+  id: number | string;
+  qty: number;
+  name?: string;
+  images?: string[]
+  category?: string
+  description?: string
+  tags?: string[];
+
+}
+
+export interface CartContextType {
+  cart: CartItem[];
+  totalUniqueItems: number
+  totalItems: number
+  addItem: (item: CartItem) => void;
+  removeItem: (id: number | string) => void;
+  updateItem: (id: number | string, qty: number) => void;
+  emptyCart: () => void;
+}
